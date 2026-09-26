502584c0db9f4a108a0ce1a796d589384b1d6048
karthi <karthikeyankarthi27695@gmail.com>
2026-09-26 18:45:09 +0530

    local changes

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,27 +1,39 @@
-import { Sequelize } from 'sequelize';
-import logger from '../lib/logger';
+import { Sequelize } from "sequelize";
+import logger from "../lib/logger";
 
-console.log(global.environment, 'from config 112');
+console.log(global.environment, "from config 112");
 
-const dbEnv = global.environment.db
+const dbEnv = global.environment.db;
 
-
-
-
-const sequelizeConnection = new Sequelize(dbEnv.database, dbEnv.user, dbEnv.password, {
+const sequelizeConnection = new Sequelize(
+  dbEnv.database,
+  dbEnv.user,
+  dbEnv.password,
+  {
     host: dbEnv.host,
-    dialect: 'mysql',
+    port: Number(dbEnv.port),
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Allows connection without manually downloading the CA cert
+      },
+    },
     pool: {
-        acquire: 500000
+      acquire: 500000,
     },
     // logging: false
-
-});
+  },
+);
 
 // Use the afterConnect hook to disable ONLY_FULL_GROUP_BY
-sequelizeConnection.addHook('afterConnect', async (connection: any) => {
-    console.log('test')
-    connection.promise().query("SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));");
+sequelizeConnection.addHook("afterConnect", async (connection: any) => {
+  console.log("test");
+  connection
+    .promise()
+    .query(
+      "SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));",
+    );
 });
 // async function initializeDatabase() {
 
@@ -34,7 +46,6 @@ sequelizeConnection.addHook('afterConnect', async (connection: any) => {
 //         console.error('Unable to connect to the database:', error);
 //     }
 // }
-
 
 // initializeDatabase();
 export default sequelizeConnection;
